09bc735b3b3b99fb3f4f5bf7d7119f24fc8a5353
Emil Ekelund <emil.ekelund@outlook.com>
2020-06-07 16:02:03 +0200

    Delete output folder from github

--- pca10040/s132/ses/Output/nRF52832_temperature_board Debug/Obj/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/nRF52832_temperature_board\ Debug/Obj/sensorsim.o: \
- /home/emil/nRF_SDK/SDK15/components/libraries/sensorsim/sensorsim.c \
- /home/emil/nRF_SDK/SDK15/components/libraries/sensorsim/sensorsim.h \
- /usr/share/segger_embedded_studio_for_arm_4.52b/include/stdint.h \
- /usr/share/segger_embedded_studio_for_arm_4.52b/include/stdbool.h
--- pca10040/s132/ses/Output/nRF52832_temperature_board Debug/Obj/ses_nRF_Startup.d
+++
@@ -1,2 +0,0 @@
-Output/nRF52832_temperature_board\ Debug/Obj/ses_nRF_Startup.o: \
- C:\Users\Lawson\ sports\Documents\Emil\nrfSDK\SDK_V15\modules\nrfx\mdk\ses_nRF_Startup.s
