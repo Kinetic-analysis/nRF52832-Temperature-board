44e74b6bffd6692c8896256d03d41fc2bf36f576
Emil Ekelund <emil.ekelund@outlook.com>
2020-06-08 08:25:49 +0200

    Remove emSession

--- pca10040/s132/ses/Output/nRF52832_temperature_board Debug/Obj/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/nRF52832_temperature_board\ Debug/Obj/sensorsim.o: \
- C:\Users\Lawson\ sports\Documents\Emil\nrfSDK\SDK_V15\components\libraries\sensorsim\sensorsim.c \
- C:\Users\Lawson\ sports\Documents\Emil\nrfSDK\SDK_V15\components\libraries\sensorsim\sensorsim.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52c/include/stdbool.h
